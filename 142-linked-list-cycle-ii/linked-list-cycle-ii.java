0f9e3e81b24dd513e56ed602d2e098ef3e505576
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2024-08-18 20:04:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (62.18%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            //Tortoise and hare Algorithm || Floyd's Cycle-Finding
+            slow = slow.next;
+            fast = fast.next.next;
+            //First time when the fast and slow pointer meets we reset slow to head.
+            if(slow==fast){
+                slow= head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
