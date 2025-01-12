5a087c109267a96d35735ee34ed8ca2c8936cd4f
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-01-12 16:43:54 +0530

    Time: 71 ms (49.40%) | Memory: 45.2 MB (19.37%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int beautySum(String s) {
+        int len = s.length();
+        int count = 0;
+
+        for(int i=0;i<len;i++){
+            int[] alp = new int[26];
+            for(int j=i;j<len;j++){
+                int index = s.charAt(j)-'a';
+                alp[index]+=1;
+                int[] minMax = findMinMax(alp);
+                count += (minMax[1] - minMax[0]);
+            }
+        }
+        return count;
+    }
+    public static int[] findMinMax(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for (int value : arr) {
+            if (value > 0) {
+                min = Math.min(min, value);
+                max = Math.max(max, value);
+            }
+        }
+        
+        return new int[]{min, max};
+    }
+}
