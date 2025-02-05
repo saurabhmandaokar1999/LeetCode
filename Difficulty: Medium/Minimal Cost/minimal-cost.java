e945dd760625c377a2dd009a6bc67166112f0857
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-02-05 16:47:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public int minimizeCost(int k, int arr[]) {
+        int len =arr.length;
+        int[] dp = new int[len+1];
+        dp[0]=0;
+        for(int i=1;i<len;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=1;j<=k;j++){
+                if(i-j>=0){
+                int jump = dp[i-j]+Math.abs(arr[i]-arr[i-j]);
+                min= Math.min(min,jump);
+                }
+            }
+            dp[i]=min;
+        }
+        return dp[len-1];
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.minimizeCost(k, arr);
+
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
