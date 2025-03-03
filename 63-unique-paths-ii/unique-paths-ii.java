2f965e8c5db519d23ed0e24ae11e31c53eb268e2
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-03 20:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (63.92%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,24 +1,26 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] oG) {
-        int r = oG.length;
-        int c = oG[0].length;
-        int[][] dp = new int[r][c];
-        for(int[] row: dp){
-            Arrays.fill(row,-1);
-        }
-        return helper(r-1,c-1,oG,dp);
-    }
-    public int helper(int r, int c,int[][]oG,int[][]dp){
-        if(r==0 && c==0 && oG[r][c]!=1){
-            return 1;
-        }
-        if(r<0 || c<0 || oG[r][c]==1){
-            return 0;
-        }
-        if(dp[r][c]!=-1) return dp[r][c];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int r = obstacleGrid.length;
+        int c =  obstacleGrid[0].length;
 
-        int up = helper(r-1,c,oG,dp);
-        int left = helper(r,c-1,oG,dp);
-        return dp[r][c] = up+left;
+        int [][] dp = new int[r][c];
+
+         for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0 && j==0 ) {dp[i][j]=1; continue;}
+
+                    int up=0;
+                    int left=0;
+                    if(i>0){up = dp[i-1][j];}
+                    if(j>0){left = dp[i][j-1];}
+                    dp[i][j]=up+left;
+            }
+         }
+         return dp[r-1][c-1];
     }
+
 }
