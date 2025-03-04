a344eac1beab5f3f47422fc95a9c0c695930875a
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-04 09:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (79.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,19 +3,20 @@ class Solution {
         int r= obstacleGrid.length;
         int c=obstacleGrid[0].length;
 
-        int[][] dp = new int[r][c];
+        int[] prev = new int[c];
 
         for(int i=0;i<r;i++){
+            int[] curr = new int[c];
             for(int j=0;j<c;j++){
-                if(obstacleGrid[i][j]==1){ dp[i][j]=0 ; continue;}
-                if(i==0 && j==0){ dp[i][j]=1; continue;}
+                if(obstacleGrid[i][j]==1){ curr[j]=0 ; continue;}
+                if(i==0 && j==0){ curr[j]=1; continue;}
                 int down= 0,right=0;
-                if(i>0){down = dp[i-1][j];}
-                if(j>0){right= dp[i][j-1];}
-             
-                dp[i][j]=down+right;
+                if(i>0){down = prev[j];}
+                if(j>0){right= curr[j-1];}
+                curr[j]=down+right;
             }
+            prev=curr;
         }
-        return dp[r-1][c-1];
+        return prev[c-1];
     }
 }
