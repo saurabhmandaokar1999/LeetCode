b6111b56927fe3098e5f1d76b9b904af46f58b1a
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-08 20:48:25 +0530

    Time: 56 ms (38.16%) | Memory: 47.2 MB (45.57%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,33 +1,39 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int tar =0,len=0;
+        int len = nums.length;
+        int sum=0;
         for(int i:nums){
-            len++;
-            tar+=i;
+            sum+=i;
         }
-        if(tar%2!=0) return false;
-
-        boolean[][] dp = new boolean[len][(tar/2)+1];
-        int NTar= tar/2;
-        for(boolean arr[]:dp){
-            Arrays.fill(arr,false);
+        if(sum%2!=0) return false;
+        int tar = sum/2;
+        // make dp array
+        boolean[][] dp = new boolean[len][tar+1];
+        
+        for(boolean[] temp:dp){
+            Arrays.fill(temp,false);
         }
+        //base case for tar=0 coloumn
         for(int i=0;i<len;i++){
             dp[i][0]=true;
         }
-        if(nums[0]<=NTar){
+
+        // base case for nums[0] if we reach at tar = nums[0] we get true
+        if(nums[0]<=tar){
             dp[0][nums[0]]=true;
         }
+
+        //fill rest of dp array
         for(int i=1;i<len;i++){
-            for(int j=1;j<=NTar;j++){
-                boolean np=dp[i-1][j];
-                boolean p=false;
+            for(int j=1;j<=tar;j++){
+                boolean np = dp[i-1][j];
+                boolean p = false;
                 if(nums[i]<=j){
                     p=dp[i-1][j-nums[i]];
                 }
-                dp[i][j]= p||np;
+                dp[i][j] = p||np;
             }
         }
-        return dp[len-1][NTar];
+        return dp[len-1][tar];
     }
 }
