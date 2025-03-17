003eb8f1bbe319f446e793ef8d9f7afb87c2dd15
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-17 23:19:43 +0530

    Time: 15 ms (77.67%) | Memory: 45.6 MB (12.73%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,20 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[][] dp = new int[coins.length][amount+1];
-        for(int[] arr: dp){
-            Arrays.fill(arr,-1);
+        for(int i=0;i<=amount;i++){
+          if(i%coins[0]==0) dp[0][i]= i/coins[0];
+          else dp[0][i] = 100000;
         }
-        int ans= helper(coins,amount,coins.length-1,dp);
-        if(ans>=100000){
-            return -1;
+        for(int idx=1;idx<coins.length;idx++){
+            for(int tar=1;tar<=amount;tar++){
+                int np=dp[idx-1][tar];
+                int p=100000;
+                if(tar>=coins[idx]){
+                    p=1+dp[idx][tar-coins[idx]];
+                }
+                dp[idx][tar]=Math.min(p,np);
+            }
         }
-        return ans;
-    }
-    public int helper(int coins[] , int tar,int idx,int[][] dp){
-        if(tar==0) return 0;
-        if(idx==0){
-            if(tar%coins[idx]==0) return tar/coins[idx];
-            else return 100000;
-        }
-        if(dp[idx][tar]!=-1) return dp[idx][tar];  
-        int np = helper(coins, tar,idx-1,dp);
-        int p = 100000;
-        if(coins[idx]<=tar){
-            p=1+helper(coins,tar-coins[idx],idx,dp);
-        }
-        return dp[idx][tar]=Math.min(p,np);
+        return dp[coins.length - 1][amount] >= 100000 ? -1 : dp[coins.length - 1][amount];
     }
 }  
