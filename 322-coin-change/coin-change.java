dca0f68ea17309ef41b10162406e2807b4a46bb0
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-17 23:28:00 +0530

    Time: 14 ms (85.26%) | Memory: 44.8 MB (45.81%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,20 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length][amount+1];
+        int[] dp = new int[amount+1];
         for(int i=0;i<=amount;i++){
-          if(i%coins[0]==0) dp[0][i]= i/coins[0];
-          else dp[0][i] = 100000;
+          if(i%coins[0]==0) dp[i]= i/coins[0];
+          else dp[i] = 100000;
         }
         for(int idx=1;idx<coins.length;idx++){
+            int[] temp = new int[amount+1];
             for(int tar=1;tar<=amount;tar++){
-                int np=dp[idx-1][tar];
+                int np=dp[tar];
                 int p=100000;
                 if(tar>=coins[idx]){
-                    p=1+dp[idx][tar-coins[idx]];
+                    p=1+temp[tar-coins[idx]];
                 }
-                dp[idx][tar]=Math.min(p,np);
+                temp[tar]=Math.min(p,np);
             }
+            dp=temp;
         }
-        return dp[coins.length - 1][amount] >= 100000 ? -1 : dp[coins.length - 1][amount];
+        return dp[amount] >= 100000 ? -1 : dp[amount];
     }
 }  
