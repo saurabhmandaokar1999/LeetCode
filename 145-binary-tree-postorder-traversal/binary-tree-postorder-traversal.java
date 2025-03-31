d21a0ef634011d97f815a19e079c4a439c3de77e
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-31 22:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (8.14%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,17 +15,23 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        //reccursive solution
-        List<Integer> list = new ArrayList<>();
-        return helper(root,list);
-    }
-    public List<Integer> helper(TreeNode root,List<Integer> list){
-        if(root==null){
-            return list;
+        //itrative solution
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+
+        List<Integer> lst = new ArrayList<>();
+
+        if(root!=null) s1.push(root);
+        
+        while(!s1.isEmpty()){
+            root=s1.pop();
+            s2.push(root);
+            if(root.left!=null) s1.push(root.left);
+            if(root.right!=null) s1.push(root.right);
         }
-        if(root.left!=null) helper(root.left,list);
-        if(root.right!=null) helper(root.right,list);
-        list.add(root.val);
-        return list;
+        while(!s2.isEmpty()){
+            lst.add(s2.pop().val);
+        }
+        return lst;
     }
 }
