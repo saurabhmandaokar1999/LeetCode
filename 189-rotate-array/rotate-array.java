50058028b04b9f4d1e24408aa8bfcab1694e21be
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-05-04 00:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 57.4 MB (66.13%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,23 +1,18 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        //this is to find effective rotation when length of k is greater than arrays length
-        k%=nums.length;
-        //rotate the array completely
-        rotate(nums,0,nums.length-1);
-        //rotate first k element
-        rotate(nums,0,k-1);
-        //rotate rest of eleemnt in array 
-        rotate(nums,k,nums.length-1);
+        int len = nums.length;
+        if(k>len) k = k%len;
+        rotate(0,len-1,nums);
+        rotate(k,len-1,nums);
+        rotate(0,k-1,nums);
     }
-
- //Function for rotating the array 
- public void rotate(int[] nums, int start, int end){
-    while(start< end){
-        int temp=nums[start];
-        nums[start]=nums[end];
-        nums[end]=temp;
-        start++;
-        end--;
+    public void rotate(int start, int end ,int[] nums){
+        while(start<end){
+            int temp = nums[start];
+            nums[start]= nums[end];
+            nums[end]= temp;
+            start++;
+            end--;
+        }
     }
-  }
 }
