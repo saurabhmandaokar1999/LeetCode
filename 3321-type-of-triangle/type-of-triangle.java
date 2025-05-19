7f414f1e783240f9bd71a964e4449a687399262c
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-05-19 18:19:59 +0530

    Time: 1 ms (34.56%) | Memory: 42.2 MB (31.99%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,26 +1,9 @@
 class Solution {
     public String triangleType(int[] nums) {
-        int sum=nums[0]+nums[1]+nums[2];
-        int equalCount=0;
-        for(int i=0;i<3;i++){
-            if(i>0){
-                if(nums[i]==nums[i-1]) equalCount++;
-            }
-            if(i>1){
-                if(nums[i]==nums[i-2] && equalCount<2) equalCount++;
-            }
-            if(!(sum-nums[i]>nums[i])) return "none";
-        }
-        switch(equalCount){
-            case 0:
-            return "scalene";
-
-            case 1:
-            return "isosceles";
-
-            case 2:
-            return"equilateral";
-        }
-        return "";
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c || a + c <= b || b + c <= a) return "none";
+        if (a == b && b == c) return "equilateral";
+        if (a == b || b == c || a == c) return "isosceles";
+        return "scalene";
     }
 }
