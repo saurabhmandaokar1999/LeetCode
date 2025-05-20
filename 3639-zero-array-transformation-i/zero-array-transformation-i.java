737539df4eda88dbb2594efe97faf123c3dfe934
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-05-20 19:32:21 +0530

    Time: 2 ms (100.00%) | Memory: 96.1 MB (23.25%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        //using Line sweep algo to solve this
-        int n = nums.length;
-        int[] arr = new int[n+1];
-        for(int[] temp : queries){
-            int i=temp[0],j=temp[1];
-            arr[i]--;
-            //here we use +1 as we want to include the number
-            arr[j+1]++;
+        int decArr[] = new int[nums.length+1];
+        for(int[] arr : queries){
+            int p = arr[0];
+            int q = arr[1];
+            decArr[p]--;
+            decArr[q+1]++;
         }
         int dec = 0;
-        for(int i=0;i<n;i++){
-            dec+=arr[i];
+        for(int i=0;i<nums.length;i++){
+            dec+=decArr[i];
             if(nums[i]+dec>0){
                 return false;
+                
             }
         }
         return true;
     }
-    //Explanation:
-    //we maintain an array of decrement as in example 2 [[1,3],[0,2]]
-    //here we can say idx 1,2 are decremented twice so we maintain  that in arr and then apply decrement later. 
 }
