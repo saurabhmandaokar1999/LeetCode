b790e6c38749fee0b24cf9fd08725f2aedf2c8fa
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-08-18 18:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (8.75%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,35 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        //convert to adj list from matrix
-        int len = isConnected.length;
-        boolean[] vis = new boolean[len];
-        int ans = 0;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<len;i++){
-            adj.add(new ArrayList<Integer>());
-        }
-
-        for(int i =0;i<len;i++){
-            for(int j =0;j<len;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
+        int col = isConnected.length;
+        int row = isConnected[0].length;
+        boolean[] vis = new boolean[col];
+        int count=0;
+        //use dfs for this
+        for(int i=0;i<row;i++){
+            if(!vis[i]){
+                dfs(isConnected,vis,col,i);
+                count++;
             }
         }
-        for(int i=0;i<len;i++){
-            if(vis[i]==false){
-                ans++;
-                dfs(adj,i,vis);
-            }
-        }
-        return ans;
+        return count;
     }
-    public void dfs(List<List<Integer>> adj,int idx,boolean[] vis){
-        for(Integer i : adj.get(idx)){
-            if(vis[i]==false){
-                vis[i]=true;
-                dfs(adj,i,vis);
+    private void dfs(int[][] isConnected,boolean[] vis, int col,int i){
+        vis[i] = true;
+        for(int j=0;j<col;j++){
+            if(!vis[j] && isConnected[i][j]==1){
+                dfs(isConnected,vis,col,j);
             }
         }
     }
