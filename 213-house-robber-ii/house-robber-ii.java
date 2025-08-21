ab920a5de2526a5c6f43f067204c8e80054edab5
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-08-21 23:15:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (91.43%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,24 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
         int len = nums.length;
-        if(len==1) return nums[len-1];
+        if(len==1) return nums[0];
         int[] dp = new int[len];
         Arrays.fill(dp,-1);
-        //picking last-idx or not picking last-idx
-        int p=helper(nums,len-1,1,dp);
-        Arrays.fill(dp,-1);
-        int np=helper(nums,len-2,0,dp);
+        int p = helper(nums,len-1,1,dp);
+         Arrays.fill(dp,-1);
+        int np = helper(nums,len-2,0,dp);
         return Math.max(p,np);
     }
-    public int helper(int[] nums,int end,int start,int[]dp){
-    if(start>end) return 0;
-    
-    if(dp[end]!=-1)return dp[end];
-    
-    int p=nums[end]+helper(nums,end-2,start,dp);
-
-    int np=helper(nums,end-1,start,dp);
-
-    return dp[end] = Math.max(p,np);
+    public int helper(int[] nums,int n,int e,int[] dp){
+        if(n<e) return 0;
+        if(n==e) return nums[e];
+        if(dp[n]!=-1) return dp[n];
+        int p=nums[n]+helper(nums,n-2,e,dp);
+        int np=helper(nums,n-1,e,dp);
+        return dp[n]=Math.max(p,np);
     } 
 }
