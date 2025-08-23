62c8563fb059d06ac3c514b70123882861e5dc16
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-08-23 10:58:13 +0530

    Time: 2 ms (82.52%) | Memory: 45.2 MB (87.69%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,24 +1,30 @@
 class Solution {
+    private static final int INF = 1_000_000_000;
     public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int next[] = new int[n];
-        for(int j=0;j<n;j++){
-            next[j] = matrix[n-1][j];
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int ans=Integer.MAX_VALUE;
+        int dp[][] = new int[row][col];
+        for(int[] d:dp){
+            Arrays.fill(d,Integer.MIN_VALUE);
         }
-        for(int i=n-2;i>=0;i--){
-            int cur[] = new int[n];
-            for(int j=0;j<n;j++){
-                int q1 = j==0 ? 1000001:next[j-1];
-                int q2 = next[j];
-                int q3 = j==n-1 ? 1000001:next[j+1];
-                cur[j] = matrix[i][j] + Math.min(q1,Math.min(q2,q3));
-            }
-            next = cur;
+        for(int i=0;i<col;i++){
+            ans=Math.min(ans,helper(0,i,matrix,row,col,dp));
         }
-        int mini = next[0];
-        for(int j=1;j<n;j++){
-            mini = Math.min(mini,next[j]);
-        }
-        return mini;
+        return ans;
     }
+    public int helper(int row,int col,int[][] matrix,int rowLen,int colLen,int[][] dp){
+        if(col<0|| col>=colLen) return INF;
+        if(row==rowLen-1) return matrix[row][col];
+        if(dp[row][col]!=Integer.MIN_VALUE) return dp[row][col];
+        int [] colPath = {-1,0,1};
+        int path=Integer.MAX_VALUE;
+        for(int c:colPath){
+            if(col+c>=0 && col+c<=colLen){
+                path=Math.min(path,matrix[row][col]+helper(row+1,col+c,matrix,rowLen,colLen,dp));
+            }
+        }
+        return dp[row][col]=path;
+    }
+
 }
