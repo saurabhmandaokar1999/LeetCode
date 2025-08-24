5e5745c6d1fb68d392cd8fcd7e5b540cedb0fa10
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-08-24 22:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (61.58%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -4,15 +4,12 @@ class Solution {
         if (n == 0) {
             return false;
         }
-        // Base case: 1 is the fundamental power of four (4^0).
         if (n == 1) {
             return true;
         }
-        // If n is not divisible by 4, it cannot be a power of four.
         if (n % 4 != 0) {
             return false;
         }
-        // Recursive step: solve the same problem for n/4.
         return isPowerOfFour(n / 4);
     }
 }
