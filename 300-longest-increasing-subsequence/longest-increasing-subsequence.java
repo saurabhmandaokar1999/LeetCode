f8b12d23109b278daee4f2b22702d95501ff2d1b
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-09-19 17:44:03 +0530

    Time: 7 ms (80.90%) | Memory: 44.6 MB (37.25%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -8,22 +8,17 @@ class Solution {
 
         for (int i = 1; i < n; i++) {
             if (arr[i] > temp.get(temp.size() - 1)) {
-                // arr[i] > the last element of temp array
-
                 temp.add(arr[i]);
                 len++;
             } else {
                 // Replacement step
                 int ind = Collections.binarySearch(temp, arr[i]);
-
                 if (ind < 0) {
                     ind = -ind - 1;
                 }
-
                 temp.set(ind, arr[i]);
             }
         }
-
         return len;
     }
 } 
