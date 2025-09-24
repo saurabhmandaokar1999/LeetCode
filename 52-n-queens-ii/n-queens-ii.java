9b3c1379cc0692fa7452c466c64f1583cefbcc2d
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-09-24 23:01:53 +0530

    Time: 4 ms (18.13%) | Memory: 39.8 MB (99.85%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,27 +1,31 @@
 class Solution {
-    private int ans=0;
     public int totalNQueens(int n) {
-
+        int ans=0;
         boolean[][] board = new boolean[n][n];
         for(int col=0;col<n;col++){
             board[0][col]=true;
-            helper(n-1,1,board,n);
+            ans+=helper(n-1,1,board,n);
             board[0][col]=false;
         } 
         return ans;
     }
-    public void helper(int queen,int row,boolean[][] board,int n){
+    public int helper(int queen,int row,boolean[][] board,int n){
         if(row==n){
-            ans++;
-            return;
-        }
-        for(int col=0;col<n;col++){
-            if(isPossible(board,n,row,col)){
-                board[row][col]=true;
-                helper(queen-1,row+1,board,n);
-                board[row][col]=false;
+            if(queen==0){
+                return 1;
+            }else{
+                return 0;
             }
         }
+        int ans=0;
+        for(int col=0;col<n;col++){
+            if(isPossible(board,n,row,col)){
+            board[row][col]=true;
+            ans+=helper(queen-1,row+1,board,n);
+            board[row][col]=false;
+            }
+        }
+        return ans;
     }
     public boolean isPossible(boolean[][] board,int n,int row,int col){
         for(int i=row;i>=0;i--){
