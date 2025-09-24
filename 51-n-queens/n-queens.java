45d8748f85b1a3c962625d9ba0401148e0b9614b
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-09-24 22:14:24 +0530

    Time: 3 ms (44.70%) | Memory: 44.9 MB (57.09%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,60 +1,64 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        String[][] board = new String[n][n];
-        for(String[] str : board){
-            Arrays.fill(str,".");
+        char[][] board = new char[n][n];
+        for(char[] c : board){
+            Arrays.fill(c,'.');
         }
-        helper(n,ans,board,0);
+        for(int col=0;col<n;col++){
+            board[0][col]='Q';
+            helper(n-1,1,n,board,ans);
+            board[0][col]='.';
+        }
         return ans;
     }
-    public void helper(int n,List<List<String>> ans, String[][] board,int col){
-        if(col == n){
-                List<String> an = new ArrayList<String>();
-                for(String[] str : board){
-                    StringBuilder sb = new StringBuilder();
-                    for(int i=0;i<n;i++){
-                        sb.append(str[i]);
-                    }
-                    an.add(sb.toString());
-                }
-                ans.add(an);
+    public void helper(int queen,int row,int n,char[][] board,List<List<String>> ans){
+        if(row==n){
+            if(queen==0){
+                List<String> temp = getString(board,n);
+                ans.add(temp);
+                return;
+            }
+            else return;
         }
-        for(int row=0;row<n;row++){
-            if(isSafe(board,row,col,n)){
-                board[row][col]="Q";
-                helper(n,ans,board,col+1);
-                board[row][col]=".";
+        for(int col=0;col<n;col++){
+            boolean canPlace = canQueenBePlaced(board,row,col,n);
+            if(canPlace){
+                board[row][col]='Q';
+                helper(queen-1,row+1,n,board,ans);
+                board[row][col]='.';
             }
         }
     }
-    public boolean isSafe(String[][] board,int row ,int col,int n){
-        int tempR= row;
-        int tempC= col;
-        while(tempR>0 && tempC>0){
-            tempR=tempR-1;
-            tempC=tempC-1;
-            if(board[tempR][tempC]=="Q"){
-                return false;
-            } 
+    public boolean canQueenBePlaced(char[][] board,int row,int col,int n){
+        for(int i=row;i>=0;i--){
+            if(board[i][col] =='Q') return false;
         }
-        tempR=row;
-        tempC=col;
-        while(tempC>0){
-            tempC=tempC-1;
-            if(board[tempR][tempC]=="Q"){
-                return false;
-            }
+        int r = row;
+        int c = col;
+        while(r>=0 && c>=0){
+            if(board[r][c]=='Q') return false;
+            r--;
+            c--;
         }
-        tempR=row;
-        tempC=col;
-        while(tempR<n-1 && tempC>0){
-            tempR=tempR+1;
-            tempC=tempC-1;
-           if(board[tempR][tempC]=="Q"){
-                return false;
-            }
+        r = row;
+        c = col;
+        while(r>=0 && c<n){
+            if(board[r][c]=='Q') return false;
+            r--;
+            c++;
         }
         return true;
+    }
+    public List<String> getString(char[][] board,int n){
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j=0;j<n;j++){
+                sb.append(board[i][j]);
+            }
+            list.add(sb.toString());
+        }
+        return list;
     }
 }
