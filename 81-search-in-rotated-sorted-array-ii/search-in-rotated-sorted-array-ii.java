253a541a937d4df9203d5fceabeb703bb71d6eea
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-10-02 18:26:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (27.71%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -4,24 +4,23 @@ class Solution {
         int right = nums.length-1;
 
         while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target){
-                return true;
-            }else if(nums[left]==nums[mid] && nums[right]==nums[mid]){
-                left +=1;
-                right-=1;
+            int mid = left+((right-left)/2);
+            if(nums[mid]==target || nums[left]==target) return true;
+            if(nums[mid]==nums[left] && nums[mid]==nums[right]){
+                left += 1;
+                right -= 1;
             }
-            else if(nums[left]<=nums[mid]){
-                if(nums[left]<=target && nums[mid]>=target){
+            else if(nums[mid]<=nums[right]){
+                if(nums[mid]<target && target <= nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }else{
+                if(nums[left]<=target && nums[mid]>target){
                     right = mid-1;
                 }else{
-                    left=mid+1;
-                } 
-            }else{
-                if(nums[right]>=target && nums[mid]<=target){
-                    left=mid+1;
-                }else{
-                    right=mid-1;
+                    left = mid+1;
                 }
             }
         }
