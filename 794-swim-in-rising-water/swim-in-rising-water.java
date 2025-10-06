b412d8dd8ce0d2015b759c6770655545a5767e9d
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-10-06 21:19:34 +0530

    Time: 4 ms (95.52%) | Memory: 44.5 MB (60.84%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -0,0 +1,40 @@
+//{
+    //There are 2 approach I can think 
+    // DFS traversal and maintain max value in that traversal
+    //after reaching end return max value found in that
+    //and return min of max value 
+    //Also because of bias thinking I know we have to apply binary search
+    //ans also the fact that 0 <= grid[i][j] < n2
+    //this is one of the condition.
+//}
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int len = grid.length;
+        int l = 0;
+        int h = (len*len)-1;
+        int ans = h+1 ;
+        while(h>=l){
+            int[][] vis = new int[len][len];
+            int mid = (l+h)/2;
+            if(dfs(grid,0,0,len-1,vis,mid)){
+                h = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+    public boolean dfs(int[][] grid,int row,int col,int n,int[][] vis,int curr){
+        if(row < 0 || col < 0 || row > n || col > n || vis[row][col]==1 || grid[row][col]>curr) return false;
+        if(row == n && col == n) return true;
+
+        vis[row][col] = 1;
+        
+        boolean up = dfs(grid,row-1,col,n,vis,curr);
+        boolean down = dfs(grid,row+1,col,n,vis,curr);
+        boolean left = dfs(grid,row,col-1,n,vis,curr);
+        boolean right = dfs(grid,row,col+1,n,vis,curr);
+        
+        return up || right || left || down;
+    }
+}
