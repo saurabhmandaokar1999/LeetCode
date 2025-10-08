c5dd1d40a8d7c12536dbe7f03c11d6c4f6625223
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-10-08 21:01:22 +0530

    Time: 69 ms (38.81%) | Memory: 61.1 MB (88.13%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -1,54 +1,31 @@
 class Solution {
-    public int[] avoidFlood(int[] rain) {
-        int n = rain.length;
-        UnionFind uf = new UnionFind(n + 1);
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] res = new int[n];
-        Arrays.fill(res, 1);
+    public int[] avoidFlood(int[] rains) {
+        int len = rains.length;
+        int [] ans = new int[len];
+        TreeSet<Integer> ts = new TreeSet<>();
+        Map<Integer,Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            int lake = rain[i];
-            if (lake == 0) continue;
-
-            res[i] = -1;
-            uf.unite(i);
-
-            if (map.containsKey(lake)) {
-                int prev = map.get(lake);
-                int dry = uf.find(prev + 1);
-
-                if (dry >= i) return new int[0];
-
-                res[dry] = lake;
-                uf.unite(dry);
-                map.put(lake, i);
-            } else {
-                map.put(lake, i);
+        for(int i=0;i<len;i++){
+            if(rains[i]==0){
+                ts.add(i);
+            }
+            else{
+                if(map.containsKey(rains[i])){
+                    Integer idx = ts.ceiling(map.get(rains[i]));
+                    if(idx == null) return new int[0];
+                    ts.remove(idx);
+                    map.put(rains[i],i);
+                    ans[idx] = rains[i];
+                    ans[i] = -1;
+                }else{
+                    map.put(rains[i],i);
+                    ans[i]=-1;
+                }
             }
         }
-
-        return res;
-    }
-}
-
-class UnionFind {
-    int[] parent;
-
-    public UnionFind(int size) {
-        parent = new int[size + 1];
-        for (int i = 0; i <= size; i++) {
-            parent[i] = i;
+        while(!ts.isEmpty()){
+            ans[ts.pollFirst()]=1;
         }
-    }
-
-    public int find(int i) {
-        if (parent[i] == i)
-            return i;
-        parent[i] = find(parent[i]);
-        return parent[i];
-    }
-
-    public void unite(int i) {
-        parent[i] = find(i + 1);
+        return ans;
     }
 }
