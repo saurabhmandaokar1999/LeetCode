4431132b4ee2265154365980374d99486f13d916
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-10-09 19:43:01 +0530

    Time: 15 ms (24.27%) | Memory: 45.2 MB (6.77%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,21 +1,14 @@
 class Solution {
-    static{    //why using this static bloack?
-        for(int i=0;i<500;i++)
-        isAnagram("","a");
-    }
-    public static boolean isAnagram(String s, String t) {
-       int count[] = new int [26];
-       if(s.length()!= t.length())
-       return false;
-       for(int i=0;i<s.length();i++){
-        count[s.charAt(i)-'a']++;
-        count[t.charAt(i)-'a']--;
-       } 
-       for(int val : count){
-        if(val != 0)
-        return false;
-       }
-       System.gc();
-       return true;
+    public boolean isAnagram(String s, String t) {
+        Map<Character , Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c : t.toCharArray()){
+            if (!map.containsKey(c)) return false;
+            map.put(c, map.get(c) - 1);
+            if (map.get(c) == 0) map.remove(c);
+        }
+        return map.size() == 0;
     }
 }
