954bfe15df88203067740b7755696e2fac2fd139
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-10-28 22:48:01 +0530

    Time: 1 ms (54.36%) | Memory: 41.7 MB (48.85%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,18 +1,13 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int o = 0, c = 0, ans = 0;
-
-        if(s.length() == 1) return 1;
-        for(char chr : s.toCharArray()){
-            if(chr == ')'){
-                if(o <= 0) ans++;
-                else {
-                    o--;
-                }
-            }else{
-                o++;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peekLast() == '(' && c == ')') {
+                stack.pollLast();
+            } else {
+                stack.offerLast(c);
             }
         }
-        return ans+o;
+        return stack.size();
     }
 }
