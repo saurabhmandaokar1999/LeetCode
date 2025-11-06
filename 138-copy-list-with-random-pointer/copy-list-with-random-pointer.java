e9a1024641abacbeb1d3976f42915203af5bd241
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-11-06 15:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (5.37%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -1,57 +1,22 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     public Node copyRandomList(Node head) {
-        Node ans = new Node(-1);
-        Node fin = ans;
-        Node temp = head;
-        int len = 0;
-        while(temp != null){
-            ans.next = new Node(temp.val);
-            ans = ans.next;
-            temp = temp.next;
-            len++;
+        Map<Node, Node> hashMap = new HashMap<>();
+        Node cur = head;
+
+        while (cur != null) {
+            hashMap.put(cur, new Node(cur.val));
+            cur = cur.next;
         }
-        //final ans for
-        fin = fin.next;
-        temp = head;
-        ans = fin;
-        while(temp != null){
-            Node ran = temp.random;
-            int dis = 0;
-            if(ran == null){
-                ans.random = null;
-                ans = ans.next;
-                temp = temp.next;
-                continue;
-            }
-            while(ran.next != null){
-                dis++;
-                ran = ran.next;
-            }
-            Node findRan = fin;
-            int count = len;
-            while(count-dis-1 > 0){
-                findRan = findRan.next;
-                count--;
-            }
-            ans.random = findRan;
-            ans = ans.next;
-            temp = temp.next;
+
+        cur = head;
+
+        while (cur != null) {
+            Node copy = hashMap.get(cur);
+            copy.next = hashMap.get(cur.next);
+            copy.random = hashMap.get(cur.random);
+            cur = cur.next;
         }
-        return fin;
+
+        return hashMap.get(head);        
     }
 }
