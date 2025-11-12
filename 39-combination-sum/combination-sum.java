0828dab60a9e027a6eeda84cf03cec6ef5c8e69c
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-11-12 20:08:37 +0530

    Time: 2 ms (87.14%) | Memory: 45.6 MB (14.56%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        helper(ans,candidates,target,0,new ArrayList<Integer>());
+        List<List<Integer>> ans = new ArrayList();
+        helper(candidates,target,ans,new ArrayList<Integer>(),0);
         return ans;
     }
-    public void helper(List<List<Integer>> ans,int[] candidates, int target,int index,List<Integer>curr){
-        if(index == candidates.length){
-            if(target==0){
-                ans.add(new ArrayList<>(curr));
-            }
+
+    public void helper(int[] candidates, int target, List<List<Integer>> ans, List<Integer> temp, int idx) {
+        if (target < 0 || idx >= candidates.length) return;
+        if (target == 0) {
+            // here we crete a deep copy of current list
+            ans.add(new ArrayList(temp));
             return;
         }
-        if(target-candidates[index]>=0){
-            curr.add(candidates[index]);
-            helper(ans,candidates,target-candidates[index],index,curr);
-            curr.remove(curr.size()-1);
+        int currInt = candidates[idx];
+        if(target-currInt >= 0 ){
+        temp.add(currInt);
+        helper(candidates, target - currInt, ans, temp, idx);
+        temp.remove(temp.size() - 1);
         }
-        helper(ans,candidates,target,index+1,curr);
+        helper(candidates, target, ans, temp, idx+1);
     }
 }
