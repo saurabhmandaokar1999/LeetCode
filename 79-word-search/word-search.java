6287b94f8e9c6ae7f5718f53c4015f9a735821c9
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-11-15 00:01:48 +0530

    Time: 1358 ms (5.02%) | Memory: 47.8 MB (5.30%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,28 +1,33 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        char[] wrd = word.toCharArray();
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==wrd[0] && helper(i,j,wrd,board,0)){
-                    return true;
+        int m = board.length;
+        int n = board[0].length;
+        boolean ans = false;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word.charAt(0)){
+                    boolean vis[][] = new boolean[m][n];
+                   if(word.length() > 1)ans = helper(board, word, i, j,"", m, n,0,vis);
+                   else return board[i][j] == word.charAt(0);
+                   if(ans) return ans;
                 }
             }
         }
-        return false;
-    }
-    public boolean helper(int i,int j,char[] wrd,char[][] board,int idx){
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]!=wrd[idx]){
-            return false;
-        }
-        if(idx==wrd.length-1) return true;
-
-        char temp = board[i][j];
-        board[i][j]='*';
-        boolean ans = helper(i+1,j,wrd,board,idx+1)||
-                      helper(i-1,j,wrd,board,idx+1)||
-                      helper(i,j+1,wrd,board,idx+1)||
-                      helper(i,j-1,wrd,board,idx+1);
-        board[i][j]=temp;
         return ans;
+    }
+    public boolean helper(char[][] board, String word, int row , int col, String s,int m, int n, int strInt,boolean vis[][]){
+        if(row < 0 || col < 0 || row >= m || col >= n) return false;
+        if(word.equals(s)) return true;
+
+        if(board[row][col] != word.charAt(strInt)) return false;
+        if(vis[row][col]) return false;
+        vis[row][col] = true;
+        boolean res =  helper(board, word, row-1, col, s+board[row][col], m, n,strInt+1,vis) ||
+        helper(board, word, row+1, col, s+board[row][col], m, n,strInt+1,vis) ||
+        helper(board, word, row, col+1, s+board[row][col], m, n,strInt+1,vis) ||
+        helper(board, word, row, col-1, s+board[row][col], m, n,strInt+1,vis);
+        vis[row][col] = false;
+        return res;
+
     }
 }
