e9a32aad6f6c70845545f426922eb1bb692b3b0e
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-11-15 18:31:22 +0530

    Time: 2 ms (84.95%) | Memory: 46.6 MB (12.45%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,64 +1,56 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        char[][] board = new char[n][n];
-        for(char[] c : board){
-            Arrays.fill(c,'.');
+        List<List<String>> ans = new ArrayList();  
+        char [][] board = new char[n][n];
+        for(char[] arr: board){
+            Arrays.fill(arr,'.');
         }
-        for(int col=0;col<n;col++){
-            board[0][col]='Q';
-            helper(n-1,1,n,board,ans);
-            board[0][col]='.';
-        }
+        placeQueen(0, n, n, ans, board);
         return ans;
     }
-    public void helper(int queen,int row,int n,char[][] board,List<List<String>> ans){
-        if(row==n){
-            if(queen==0){
-                List<String> temp = getString(board,n);
-                ans.add(temp);
-                return;
+    public void placeQueen(int row, int q, int n, List<List<String>> ans, char [][] board){
+        if(row >=n ){
+            if(q==0){
+                ansHelper(board, ans);
             }
-            else return;
+            return;
         }
-        for(int col=0;col<n;col++){
-            boolean canPlace = canQueenBePlaced(board,row,col,n);
-            if(canPlace){
-                board[row][col]='Q';
-                helper(queen-1,row+1,n,board,ans);
-                board[row][col]='.';
+        for(int i=0; i<n; i++){
+            if(canPlace(board, row, i, n)){
+                board[row][i] ='Q';
+                placeQueen(row+1, q-1, n, ans, board);
+                board[row][i] ='.';
             }
         }
     }
-    public boolean canQueenBePlaced(char[][] board,int row,int col,int n){
-        for(int i=row;i>=0;i--){
-            if(board[i][col] =='Q') return false;
+    public void ansHelper(char[][] board , List<List<String>> ans){
+        List<String> curr = new ArrayList();
+        for(char[] c : board){
+            curr.add(new String(c));
         }
+        ans.add(curr);
+    }
+    public boolean canPlace(char[][] board, int row, int col,int n){
         int r = row;
         int c = col;
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q') return false;
+        while(r >=0 && c >=0){
+            if(board[r][c] == 'Q') return false;
             r--;
-            c--;
         }
         r = row;
         c = col;
-        while(r>=0 && c<n){
-            if(board[r][c]=='Q') return false;
+        while(c >=0 && r >=0){
+            if(board[r][c] == 'Q') return false;
+            c--;
             r--;
+        }
+        r = row;
+        c = col;
+        while(r >=0 && c < n ){
+            if(board[r][c] == 'Q') return false;
             c++;
+            r--;
         }
         return true;
-    }
-    public List<String> getString(char[][] board,int n){
-        List<String> list = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            StringBuilder sb = new StringBuilder();
-            for(int j=0;j<n;j++){
-                sb.append(board[i][j]);
-            }
-            list.add(sb.toString());
-        }
-        return list;
     }
 }
