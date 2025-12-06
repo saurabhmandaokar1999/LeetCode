b19c5311cefb5e5f6aa6f86f465886d67cddd190
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-12-06 21:54:00 +0530

    Time: 1 ms (70.10%) | Memory: 44.2 MB (16.28%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> subset = new ArrayList<>();
-
-        createSubset(nums, 0, res, subset);
-        return res;        
-    }
-
-    private static void createSubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
-        if (index == nums.length) {
-            res.add(new ArrayList<>(subset));
-            return;
+        List<List<Integer>> ans = new ArrayList<>();
+        int combi = (int)Math.pow(2, nums.length)-1;
+        for(int i = 0; i<=combi ;i++){
+            List<Integer> temp  = new ArrayList<>();
+            int count = 0;
+            int curr = i;
+            while(curr != 0){
+                if((curr & 1 )== 1 ){
+                    temp.add(nums[count]);
+                }
+                curr >>= 1;
+                count++;
+            }
+            ans.add(temp);
         }
-
-        subset.add(nums[index]);
-        createSubset(nums, index + 1, res, subset);
-
-        subset.remove(subset.size() - 1);
-        createSubset(nums, index + 1, res, subset);
-    }    
+        return ans;
+    }
 }
