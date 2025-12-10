594270b7373020c2b0d022768541c33b6347f667
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-12-10 19:30:45 +0530

    Time: 1 ms (44.27%) | Memory: 47 MB (11.92%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,28 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int[][] ans = image;
-        int tarColor = image[sr][sc];
-        int delRow[] = {-1,0,+1,0};
-        int delCol[] = {0,+1,0,-1};
-        dfs(image,ans,sr,sc,color,tarColor,delRow,delCol);
-        return ans;
-    }
-    public void dfs(int[][]image,int[][]ans,int sr, int sc,int color ,int tarColor,int[] delRow,int[] delCol){
-        int lenR = image.length;
-        int lenC = image[0].length;
-        ans[sr][sc]=color;
-        for(int i=0;i<4;i++){
-            int r =sr+delRow[i];
-            int c =sc+delCol[i];
-            if(r<lenR && c<lenC && r>=0 && c>=0 && image[r][c]==tarColor && ans[r][c]!=color){
-                dfs(image,ans,r,c,color,tarColor,delRow,delCol);
+        int m = image.length;
+        int n = image[0].length;
+        boolean[][] vis = new boolean[m][n];
+
+        int[][] dir = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+        int target = image[sr][sc];
+        Queue<Integer[]> q = new ArrayDeque<>();
+        q.offer(new Integer[]{sr,sc});
+        image[sr][sc] = color;
+        while(!q.isEmpty()){
+            Integer[] cordinates = q.poll();
+            int r = cordinates[0];
+            int c = cordinates[1];
+            for(int[] d : dir){
+                int row = r+d[0];
+                int col = c+d[1];
+                if(row >= 0 && col >=0 && row < m && col < n && image[row][col] == target && !vis[row][col]){
+                    image[row][col] = color;
+                    vis[row][col] = true;
+                    q.offer(new Integer[]{row, col});
+                }
             }
         }
+        return image;
     }
 }
