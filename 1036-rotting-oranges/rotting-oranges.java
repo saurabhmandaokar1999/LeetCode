8972eece7430ac00b570da6de7c75082c1014b03
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-12-10 18:59:59 +0530

    Time: 2 ms (81.59%) | Memory: 44.1 MB (46.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,49 +1,40 @@
 class Solution {
-    public static class Pair{
-        int x=0;
-        int y=0;
-        public Pair(int x , int y){
-            this.x=x;
-            this.y=y;
-        }
-    }
     public int orangesRotting(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int fresh=0;
-        int time=0;
-        Queue<Pair> q = new ArrayDeque<>();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]==2){
-                    q.offer(new Pair(i,j));
-                }else if(grid[i][j]==1){
-                    fresh++;
-                }
+        int time = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<Integer[]> q = new ArrayDeque<>();
+        int dir[][] = new int[][] {{-1,0},{1,0},{0,-1},{0,1}};
+        int fresh = 0;
+    
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1) fresh++;
+                if(grid[i][j] == 2) q.offer(new Integer[]{i,j});
             }
         }
-        if(fresh==0) return 0;
-        int[] mr = {0,1,0,-1};
-        int[] mc = {-1,0,1,0};
+
+        //BFS on grid
         while(!q.isEmpty()){
-            ++time;
-            int len = q.size();
-            for(int a=0;a<len;a++){
-                Pair p = q.poll();
-                for(int c=0;c<4;c++){
-                    int cr=p.x+mr[c];
-                    int cc=p.y+mc[c];
-                    if(cr>=0 && cr<row && cc>=0 && cc<col){
-                        if(grid[cr][cc]==2 || grid[cr][cc]==0) continue;
-                        else{
-                            grid[cr][cc]=2;
-                            q.offer(new Pair(cr,cc));
-                            fresh--;
-                        }
-                    }
+            boolean isFresh = false;
+            int size = q.size();
+            for(int p = 0; p < size; p++){
+                Integer curr[] = q.poll();
+                int i = curr[0];
+                int j = curr[1];
+                for(int[] d : dir){
+                    int row = i+d[0];
+                    int col = j+d[1];
+                    if(row >= 0 && col >= 0 && row<m && col<n && grid[row][col] == 1){
+                        q.offer(new Integer[]{row, col});
+                        isFresh = true;
+                        grid[row][col] = 2;
+                        fresh--;
+                    } 
                 }
             }
+            if(isFresh) time++;
         }
-        return fresh==0 ? time-1:-1;
+        return fresh == 0 ? time : -1;
     }
 }
