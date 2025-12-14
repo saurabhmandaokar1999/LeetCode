ba9159ec33e05118afdb89717f33262833998e58
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-12-14 13:49:48 +0530

    Time: 2 ms (99.20%) | Memory: 43.8 MB (19.91%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,18 +1,13 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        //This is the simplest soluton
-        // we maintain an integer open 
-        //This integer  is used to check if there is more than 1 open bracket then only append the current bracket else 
-        // skip the character significanlty reduces space and makes logic simple
         int open = 0;
         StringBuilder sb = new StringBuilder();
-        
         for(char c : s.toCharArray()){
             if(c=='('){
                 open++;
                 if(open > 1) sb.append(c);
             }else{
-                if(open>1) sb.append(c);
+                if(open > 1) sb.append(c);
                 open--;
             }
         }
