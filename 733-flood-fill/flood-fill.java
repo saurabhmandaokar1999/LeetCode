2a674a9c52795fc9d8894142ef95453759625f4a
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-12-29 14:56:11 +0530

    Time: 1 ms (44.34%) | Memory: 46.8 MB (58.70%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        //Using dfs or reccursion
+        int[][] dir = new int[][]{{0,1},{0,-1},{-1,0},{1,0}};
         int target = image[sr][sc];
         int m = image.length;
         int n = image[0].length;
-        int[][] dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-        boolean vis[][] = new boolean[m][n];
-        Queue<Pair<Integer,Integer>> q = new ArrayDeque<>();
-        image[sr][sc] = color;
-        q.offer(new Pair<>(sr,sc));
-        while(!q.isEmpty()){
-            int x = q.peek().getKey();
-            int y = q.peek().getValue();
-            q.poll();
-            for(int[] d : dir ){
+        boolean[][] vis = new boolean[m][n];
+        dfs(sr, sc, m, n, image, color, target, dir, vis);
+        return image;
+    }
+    public void dfs(int x, int y,int m, int n, int[][] image, int color, int target, int[][] dir, boolean[][] vis){
+        if(x >=0 && y >= 0 && x < m && y < n && image[x][y] == target && !vis[x][y]){
+            image[x][y] = color;
+            vis[x][y] = true;
+            for(int[] d : dir){
                 int a = x+d[0];
                 int b = y+d[1];
-                if(a>=0 && b>=0 && a < m && b < n && image[a][b] == target && !vis[a][b]){
-                    q.offer(new Pair<>(a,b));
-                    image[a][b] = color;
-                    vis[a][b] = true;
-                }
+                dfs(a,b,m,n,image,color,target,dir,vis);
             }
         }
-        return image;  
     }
 }
