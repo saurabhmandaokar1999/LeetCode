7c9da9394e0a5de17cb2c29c4dbe3f2eb7c49b11
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2026-01-05 18:46:34 +0530

    Time: 7 ms (52.29%) | Memory: 58.2 MB (5.08%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,39 +1,22 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int len = nums.length;
+        int requiredLen = len/3;
+        //sorting attay wil take nlog(n)
+        Arrays.sort(nums);
         List<Integer> ans = new ArrayList<>();
-        int count1 = 0, count2 = 0;
-        int candi1 = 0, candi2 = 0;
-
-        // First pass
-        for (int num : nums) {
-            if (num == candi1) {
-                count1++;
-            } else if (num == candi2) {
-                count2++;
-            } else if (count1 == 0) {
-                candi1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candi2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        int count = 1;
+        for(int i = 1; i < len ; i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            }else{
+                if(count > requiredLen) ans.add(nums[i-1]);
+                count=1;
             }
         }
-
-        // Second pass
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == candi1) count1++;
-            else if (num == candi2) count2++;
+        if(count > requiredLen){
+            ans.add(nums[len-1]);
         }
-
-        int threshold = nums.length / 3;
-        if (count1 > threshold) ans.add(candi1);
-        if (count2 > threshold) ans.add(candi2);
-
         return ans;
     }
 }
