42727bdefb9288e8d46be46229d1c05b6831d5c9
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2026-02-07 15:27:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.94%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,20 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
         int len = nums.length;
-        if(len==1) return nums[0];
-        int[] dp = new int[len];
-        Arrays.fill(dp,-1);
-        int p = helper(nums,len-1,1,dp);
-         Arrays.fill(dp,-1);
-        int np = helper(nums,len-2,0,dp);
-        return Math.max(p,np);
+        if(len ==1) return nums[0];
+        if(len < 3) return Math.max(nums[0],nums[1]);
+        Integer[] dp = new Integer[len];
+        int idx = len-1;
+        int l = nums[idx] + helper(idx-2,1,nums,dp);
+        dp =  new Integer[len];
+        int f = helper(idx-1,0, nums,dp);
+        return Math.max(l,f);                  
     }
-    public int helper(int[] nums,int n,int e,int[] dp){
-        if(n<e) return 0;
-        if(n==e) return nums[e];
-        if(dp[n]!=-1) return dp[n];
-        int p=nums[n]+helper(nums,n-2,e,dp);
-        int np=helper(nums,n-1,e,dp);
-        return dp[n]=Math.max(p,np);
-    } 
+    private int helper(int idx,int end,int[] nums,Integer [] dp){
+        if(idx == end) return nums[end];
+        if(idx < end) return 0;
+        if(dp[idx] != null) return dp[idx];
+        int p = nums[idx]+ helper(idx-2,end, nums,dp);
+        int np = helper(idx-1,end,nums,dp);
+        return dp[idx]=Math.max(p,np);
+    }
 }
