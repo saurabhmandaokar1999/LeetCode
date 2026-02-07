05eddaecc741fda2701ece6f4e79da0993407964
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2026-02-07 16:45:39 +0530

    Time: 1 ms (99.62%) | Memory: 49.6 MB (96.91%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,24 +1,17 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int row=grid.length;
-        int col=grid[0].length;
-        int[][] dp = new int[row][col];
-        dp[0][0]=grid[0][0];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(i==0){
-                    if(j>0) dp[0][j]=grid[i][j]+dp[0][j-1];
-                }
-                if(j==0){
-                    if(i>0) dp[i][0]=grid[i][j]+dp[i-1][0];
-                }
-                if(i>0 && j>0){
-                    int d=grid[i][j]+dp[i-1][j];
-                    int r=grid[i][j]+dp[i][j-1];
-                    dp[i][j]=Math.min(d,r);
-                }    
-            }
-        }
-        return dp[row-1][col-1];
+    public int minPathSum(int[][] obstacleGrid) {
+        int r = obstacleGrid.length;
+        int c = obstacleGrid[0].length;
+        Integer[][] cache = new Integer[r][c];
+        return helper(r-1,c-1,obstacleGrid,cache);
+    }
+
+    private int helper(int r,int c,int[][] obstacleGrid,Integer[][] cache){
+        if(r<0 || c<0 ) return 200*200;
+        if(r==0 && c==0) return obstacleGrid[r][c];
+        if(cache[r][c] != null) return cache[r][c];
+        int l = obstacleGrid[r][c]+helper(r,c-1,obstacleGrid,cache);
+        int u = obstacleGrid[r][c]+helper(r-1,c,obstacleGrid,cache);
+        return cache[r][c]=Math.min(l,u);
     }
 }
